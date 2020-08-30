d5af7dd907498350eab602fc869d51c365073816
loki40316 <loki40316@gmail.com>
2020-08-30 19:07:46 +0800

    eslint test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 const server = app.listen(port, () => {
-  console.log(`Getsport API listening at http://localhost:${port}`);
-});
-aa
+  console.log(`Getsport API listening at http://localhost:${port}`)
+})
+server.keepAliveTimeout = 60 * 1000
